eff26ad5eb29f36ff608f2455b4d2d993c3f25c7
daeyong <ldy5683@naver.com>
2021-04-12 11:28:55 +0900

    logs 추가

--- config/production.py
+++ config/production.py
@@ -1,5 +1,29 @@
+from logging.config import dictConfig
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'%\xeb\x07\xd4f\x8a\xad\xf7\xec\x92F\xac\xa7Ax\x1e'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root':{
+        'level':'INFO',
+        'handlers':['file']
+    }
+})
